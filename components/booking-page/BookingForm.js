8ef0efda6a7a6f46b67bba7c81b9b4fb2c31eaed
richdweb <rd.codex@outlook.com>
2022-12-23 17:04:54 +0000

    Change the input around

--- components/booking-page/BookingForm.js
+++ components/booking-page/BookingForm.js
@@ -7,9 +7,9 @@ import ReCAPTCHA from "react-google-recaptcha"
 const schema = z.object({
     firstName: z.string().min(1, { message: "Please enter your first name."}),
     surname: z.string().min(1, { message: "Please enter your surname."}),
-    phonenumber: z.string().min(11, { messgae: "Please enter a valid Phone Number."}),
-    startDestination: z.string().min(1, { message: "Please enter a Start Destination."}),
-    endDestination: z.string().min(1, { message: "Please enter a End Destination."}),
+    phonenumber: z.string().min(11, { message: "Please enter a valid phone number."}),
+    startDestination: z.string().min(1, { message: "Please enter a start destination."}),
+    endDestination: z.string().min(1, { message: "Please enter a end destination."}),
     passengers: z.string(),
     luggage: z.string(),
     oneWayDate: z.string().min(1, { message: "Please enter a pickup date."}),
@@ -25,16 +25,14 @@ export default function BookingForm()  {
         resolver: zodResolver(schema)
     })
 
-    const [ isReturn, setIsReturn ] = useState(false)
-    const [ result, setResult ]     = useState('')
-    
+    const [ result, setResult ] = useState('')
     const reRef = useRef()
 
     const handleResults = async (formValues) => {
 
-        // ReCapture token
         const token = await reRef.current.executeAsync()
         reRef.current.reset()
+
         const url = '/api/booking'
 
         const bookingDataObj = {
@@ -54,10 +52,7 @@ export default function BookingForm()  {
             })
 
             const data = await res.json()
-
             setResult(data.message)
-
-            console.log(data.message)
 
         } catch (error) {
             console.lof(error)
@@ -82,28 +77,26 @@ export default function BookingForm()  {
                 flex-col">
 
             {/* 1st */}
-            <div className="flex md:flex-row flex-col w-full justify-between relative">
+            <div className="flex md:flex-row flex-col w-full justify-between relative md:mt-0 mt-20">
 
                 {/* Firstname */}
                 <div className="flex flex-col px-4 py-2 w-full">
-                    <label htmlFor="firstName"  className='sr-only lg:not-sr-only'>First Name</label>
+                    <label htmlFor="firstName">First Name</label>
                     <input 
                         id="firstName"
                         type="text" 
                         {...register('firstName')}
-                        placeholder="First Name" 
                         className="mt-1 border p-2 rounded outline-none w-full h-14 lg:w-[514px]"/>
                         <span className="text-red-500">{errors.firstName?.message}</span>
                 </div>
 
                 {/* Surname */}
                 <div className="flex flex-col px-4 py-2 w-full">
-                    <label htmlFor="firstName"  className='sr-only lg:not-sr-only'>Surname</label>
+                    <label htmlFor="firstName">Surname</label>
                     <input 
                         id="surname"
                         type="text" 
                         {...register('surname')}
-                        placeholder="Surname" 
                         className="mt-1 border p-2 rounded outline-none w-full h-14 lg:w-[514px]"/>
                         <span className="text-red-500">{errors.surname?.message}</span>
                 </div>
@@ -115,24 +108,22 @@ export default function BookingForm()  {
 
                 {/* Start Destination */}
                 <div className="flex flex-col px-4 py-2 w-full">
-                    <label htmlFor="startDestination"  className='sr-only lg:not-sr-only'>Start Destination</label>
+                    <label htmlFor="startDestination">Start Destination</label>
                     <input 
                         id="startDestination"
                         type="text" 
                         {...register('startDestination')}
-                        placeholder="Start Destination" 
                         className="mt-1 border p-2 rounded outline-none w-full h-14 lg:w-[514px]"/>
                         <span className="text-red-500">{errors.startDestination?.message}</span>
                 </div>
 
                 {/* End Destination */}
                 <div className="flex flex-col px-4 py-2 w-full">
-                    <label htmlFor="endDestination" className='sr-only lg:not-sr-only'>End Destination</label>
+                    <label htmlFor="endDestination">End Destination</label>
                     <input 
                         id="endDestination"
                         type="text" 
                         {...register("endDestination")}
-                        placeholder="End Destination" 
                         className="mt-1 border p-2 rounded outline-none w-full h-14 lg:w-[514px]"/>
                         <span className="text-red-500">{errors.endDestination?.message}</span>
                 </div>
@@ -143,12 +134,11 @@ export default function BookingForm()  {
 
                 {/* Phonenumber */}
                 <div className="flex flex-col px-4 py-2 w-full">
-                    <label htmlFor="phonenumber" className="sr-only lg:not-sr-only">Phone Number</label>
+                    <label htmlFor="phonenumber">Phone Number</label>
                     <input 
                         id="phonenumber"
                         type="number" 
                         {...register("phonenumber")}
-                        placeholder="Phone Number" 
                         className="mt-1 border p-2 rounded outline-none w-full h-14 lg:w-[514px]"/>
                         <span className="text-red-500">{errors.phonenumber?.message}</span>
                 </div>
@@ -158,11 +148,10 @@ export default function BookingForm()  {
 
                     {/* Number of Passengers */}
                     <div className="flex flex-col py-2 w-full">
-                        <label htmlFor="passengers" className="sr-only lg:not-sr-only">Passengers</label>
+                        <label htmlFor="passengers">Passengers</label>
                         <select 
                             id="passengers"
                             {...register('passengers')}
-                            placeholder="Passengers" 
                             className="mt-1 border px-2 rounded outline-none w-full h-14 bg-white ">
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -174,11 +163,10 @@ export default function BookingForm()  {
 
                     {/* Number of Luggage */}
                     <div className="flex flex-col py-2 w-full lg:pr-2">
-                        <label htmlFor="luggage" className="sr-only lg:not-sr-only">Luggage</label>
+                        <label htmlFor="luggage">Luggage</label>
                         <select 
                             id="luggage"
                             {...register("luggage")}
-                            placeholder="Luggage" 
                             className="mt-1 border p-2 rounded outline-none w-full h-14 bg-white">
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -191,107 +179,72 @@ export default function BookingForm()  {
             </div>
 
             {/* 4th */}
-            <div className="flex flex-col lg:mt-6 mt-4 mx-4">
-                <p>Select Time and Date of Pickup</p>
-                <div className="flex flex-row space-x-1 mr-4 mt-1">   
-                    {/* Date Picker */}
-                    <div className="w-full">
-                        <label className="hidden" htmlFor="oneWayDate">Select One way date</label>
-                        <input 
-                            {...register("oneWayDate")}
-                            id="oneWayDate"
-                            type="date" 
-                            className="
-                                w-full
-                                h-14 
-                                p-2
-                                border
-                                rounded
-                                outline-none" />
-                        <span className="text-red-500">{errors.oneWayDate?.message}</span>
-                    </div>
+            <div className="flex md:flex-row flex-col w-full justify-between relative lg:mt-6">
 
-                    {/* Time Input */}
-                    <div className="w-full">
-                        <label htmlFor="onewayTime" className="sr-only">Select Start Time</label>
+                {/* Date Picker */}
+                <div className="flex flex-col px-4 py-2 w-full md:mt-0 mt-2">
+                    <label htmlFor="oneWayDate">Pickup Date</label>
+                    <input 
+                        {...register("oneWayDate")}
+                        id="oneWayDate"
+                        type="date" 
+                        className="mt-1 border p-2 rounded outline-none w-full h-14 lg:w-[514px]" />
+                        <span className="text-red-500">{errors.oneWayDate?.message}</span>
+                </div>
+
+                {/* Time Picker */}
+                <div className="flex flex-col px-4 py-2 w-full md:mt-0 mt-2">
+                        <label htmlFor="onewayTime">Pickup Time</label>
                         <input 
                             {...register("oneWayTime")}
                             id="onewayTime"
                             type="time" 
-                            className="
-                                w-full
-                                h-14 
-                                p-4
-                                border
-                                rounded
-                                outline-none"/>
-                        <span className="text-red-500">{errors.oneWayTime?.message}</span>
-                    </div>
+                            className="mt-1 border p-2 rounded outline-none w-full h-14 lg:w-[514px]"/>
+                            <span className="text-red-500">{errors.oneWayTime?.message}</span>
+                </div>
+            </div>
+
+            {/* 5th */}
+            <div className="flex md:flex-row flex-col w-full justify-between relative lg:mt-6">
+
+                {/* Return Date Picker */}
+                <div className="flex flex-col px-4 py-2 w-full md:mt-0 mt-2">
+                        <label htmlFor="returnDate">Return Date <span className="text-xs text-black/80">(Leave blank if no return is needed)</span></label>
+                        <input 
+                            {...register("returnDate")}
+                            id="returnDate"
+                            type="date" 
+                            className="mt-1 border p-2 rounded outline-none w-full h-14 lg:w-[514px]" />
+                            
+                </div>
+
+                {/* Return Time Picker */}
+                <div className="flex flex-col px-4 py-2 w-full md:mt-0 mt-2">
+                    <label htmlFor="returnTime">Return Time <span className="text-xs text-black/80">(Leave blank if no return is needed)</span></label>
+                    <input 
+                        {...register("returnTime")}
+                        id="returnTime"
+                        type="time" 
+                        className="mt-1 border p-2 rounded outline-none w-full h-14 lg:w-[514px]"/>
                 </div>
             </div>
             
-
-            {/* 5th */}
-            {
-                isReturn && (
-                    <div className="flex flex-col lg:mt-6 mt-4 mx-4">
-                        <p>Select Return Time and Date</p>
-                        <div className="flex flex-row space-x-1 mr-4 mt-1">   
-                            {/* Date Picker */}
-                            <div className="w-full">
-                                <label className="hidden" htmlFor="returnDate">Return Date</label>
-                                <input 
-                                    {...register("returnDate")}
-                                    id="returnDate"
-                                    type="date" 
-                                    className="
-                                        w-full
-                                        h-14 
-                                        p-2
-                                        border
-                                        rounded
-                                        outline-none" />
-                            </div>
-
-                            {/* Time Input */}
-                            <div className="w-full">
-                                <label htmlFor="returnTime" className="sr-only">Return Time</label>
-                                <input 
-                                    {...register("returnTime")}
-                                    id="returnTime"
-                                    type="time" 
-                                    className="
-                                        w-full
-                                        h-14 
-                                        p-4
-                                        border
-                                        rounded
-                                        outline-none"/>
-                            </div>
-                        </div>
-                    </div>
-                )
-            }
-            
-            <p 
-                onClick={() => setIsReturn(!isReturn)}
-                className="mx-4 mt-6 cursor-pointer hover:text-main-yellow">
-                {`${ !isReturn ? 'Need A return? Click here.' : "Don't need a return "}`}
-            </p>
-
              {/* 6th */}
             <div className="flex flex-col p-4 lg:mt-6">
-                <label htmlFor="message"  className='sr-only'>Message</label>
+
+                {/* Message */}
+                <label htmlFor="message">Special Requests</label>
                 <textarea 
                     id="message"
                     rows={10}
                     {...register('message')}
-                    placeholder="Write Message" 
                     className="mt-1 border p-2 rounded outline-none w-full "/>
                     <span className="text-red-500">{errors.message?.message}</span>
             </div>
 
+            {/* 7th */}
             <div className="mx-4">
+                {/* ReCaptcha */}
                 <ReCAPTCHA 
                     sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
                     size="invisible"
@@ -307,7 +260,6 @@ export default function BookingForm()  {
                 </button>
             </div>
 
-            
         </form>
     )
 }
--- pages/booking/index.js
+++ pages/booking/index.js
@@ -3,7 +3,7 @@ import Booking from "../../components/booking-page/Booking"
 
 export default function BookingPage() {
     return (
-        <div className="bg-[#F5F5F5] h-full w-full pb-20">
+        <div className="bg-[#F5F5F5] h-full w-full md:pb-20">
             <Hero />
             <Booking />
         </div>
