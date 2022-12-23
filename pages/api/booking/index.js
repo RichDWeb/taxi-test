93d638361457f014d136e8516c25ca1204ef1067
richdweb <rd.codex@outlook.com>
2022-12-23 14:20:48 +0000

    Nodemailer connection verify

--- pages/api/booking/index.js
+++ pages/api/booking/index.js
@@ -56,6 +56,18 @@ export default async function handle(req, res) {
             secure: true
         })
 
+        await new Promise((resolve,reject) => {
+            transporter.verify(function (error, success) {
+                if(error) {
+                    console.log(error)
+                    reject(error)
+                } else {
+                    console.log("Server is ready to take our message")
+                    resolve(success)
+                }
+            })
+        }) 
+
         const mailOptions = {
             from: 'localcabsemail@gmail.com',
             to: 'rd.codex@outlook.com',
